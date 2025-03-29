ec87c357588a6e1186b0a8979d789ad1592320b9
rohit <rohit.880207@gmail.com>
2025-03-29 21:20:47 +0530

    removed colors for light mode

--- src/floatingDock.tsx
+++ src/floatingDock.tsx
@@ -70,7 +70,7 @@ const FloatingDockMobile = ({
                   target='_blank'
                   href={item.href}
                   key={item.title}
-                  className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
+                  className="h-10 w-10 rounded-full bg-neutral-900 flex items-center justify-center"
                   
                 >
                   <div className="h-4 w-4">{item.icon}</div>
@@ -82,9 +82,9 @@ const FloatingDockMobile = ({
       </AnimatePresence>
       <button
         onClick={() => setOpen(!open)}
-        className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-800 flex items-center justify-center"
+        className="h-10 w-10 rounded-full bg-neutral-800 flex items-center justify-center"
       >
-        <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
+        <IconLayoutNavbarCollapse className="h-5 w-5  text-neutral-400" />
       </button>
     </div>
   );
