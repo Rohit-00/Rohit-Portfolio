e94608dce2b1736404d5a8f43bb6115570654b2e
rohit <rohit.880207@gmail.com>
2025-03-29 20:57:57 +0530

    minor changes

--- src/hooks/lenis.ts
+++ src/hooks/lenis.ts
@@ -4,13 +4,11 @@ import Lenis from '@studio-freight/lenis';
 const useLenisScroll = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      lerp: 0.1, // The lower the number, the smoother the scrolling
+      lerp: 0.1, 
       smoothWheel:true
     });
 
     lenis.on('scroll', (e: any) => {
-      // You can listen for scroll events if needed
-      console.log(e);
     });
 
     const raf = (time: number) => {
